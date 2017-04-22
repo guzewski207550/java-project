49b73145e9737aa3a4eaa0e7290b2c08d2f9c3af
MAKOLAB-NET\mateusz.guzewski <mateusz.guzewski@makolab.com>
2017-04-22 23:10:51 +0200

    Minor fixes in controller class.

--- application/src/main/java/pl/lodz/java/budget/planning/Controller.java
+++ application/src/main/java/pl/lodz/java/budget/planning/Controller.java
@@ -1,12 +1,15 @@
 package pl.lodz.java.budget.planning;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @org.springframework.stereotype.Controller
+@RequestMapping("/")
 public class Controller {
 
-    @RequestMapping("/")
-    public String hello() {
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello(Model model) {
         return "hello";
     }
 }
